cb52a4422d07a82fa8b5b9db33ee764aabe7bfc0
Gabriel Lyon <glyon1@uc.cl>
2021-08-18 17:15:55 -0400

    feat(with-events)

--- app/javascript/components/daily-co-tile.vue
+++ app/javascript/components/daily-co-tile.vue
@@ -1,13 +1,8 @@
 <template>
   <div>
-    DAILY CO TILE
-
-    <video
-      auto-play
-      muted
-      plays-inline
-      :src="videoSrc"
-    />
+    {{ this.userId }}
+    <video autoPlay muted playsInline :ref="`${this.userId}-video`" />
+    <audio autoPlay playsInline :ref="`${this.userId}-audio`" />
   </div>
 </template>
 <script>
@@ -16,12 +11,16 @@ export default {
   name: 'DailyCoTile',
   components: {},
   props: {
+    userId: { type: String, default: null },
     videoTrackState: { type: Object, default: null },
     audioTrackState: { type: Object, default: null },
     isLocalPerson: { type: Boolean, default: false },
   },
   data() {
-    return {};
+    return {
+      // videoTrackState: null,
+      // audioTrackState: null,
+    };
   },
   computed: {
     videoTrack() {
@@ -40,13 +39,22 @@ export default {
 
   },
   methods: {
+    getVideoComponent() {
+      // return videoTrack ? <video autoPlay muted playsInline ref={videoEl} /> : null;
+    },
   },
   created() {
   },
-//   watch() {
-//       videoTrack() {
-//         this.$refs('videoEl').current.srcObject = new MediaStream([this.videoTrack]);
-//       },
-//   },
+  watch: {
+    videoTrack(newVideoTrackValue) {
+      // debugger;
+      this.$refs[`${this.userId}-video`].srcObject = new MediaStream([newVideoTrackValue]);
+    },
+    audioTrack(newAudioTrackValue) {
+      if (!this.isLocalPerson) {
+        this.$refs[`${this.userId}-audio`].srcObject = new MediaStream([newAudioTrackValue]);
+      }
+    },
+  },
 };
 </script>
